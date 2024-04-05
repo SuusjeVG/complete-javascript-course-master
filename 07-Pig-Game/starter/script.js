2e44133a7941f4eb14cb6b31253453ed7dbeb4a0
i518778 <suusjevanglabbeek@live.nl>
2024-04-05 15:45:27 +0200

    dedstructering data part 1

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -32,17 +32,16 @@ const current_score_player_1 = document.getElementById('current--1');
 
 const score_player_1 = document.getElementById('score--1');
 
-
 // player 2
 let player_2_name = document.getElementById('name--2');
-const player_2 = document.querySelector('.player--2')
-const current_score_player_2 = document.getElementById('current--2')
+const player_2 = document.querySelector('.player--2');
+const current_score_player_2 = document.getElementById('current--2');
 
-const score_player_2 = document.getElementById('score--2')
+const score_player_2 = document.getElementById('score--2');
 
 function playerName() {
-  const name_one = prompt("Name player 1:");
-  const name_two = prompt("Name player 2:");
+  const name_one = prompt('Name player 1:');
+  const name_two = prompt('Name player 2:');
 
   if (name_one || name_two) {
     player_1_name.textContent = name_one;
@@ -50,36 +49,34 @@ function playerName() {
   }
 }
 
-playerName()
+playerName();
 
 // active player
 let active_player = {
   player_1: true,
   player_2: false,
-}
+};
 
 function winGame() {
-  let score1 = parseInt(score_player_1.textContent); 
-  let score2 = parseInt(score_player_2.textContent); 
+  let score1 = parseInt(score_player_1.textContent);
+  let score2 = parseInt(score_player_2.textContent);
 
-  if (score1 >= winner_score || score2 >= winner_score) { 
+  if (score1 >= winner_score || score2 >= winner_score) {
     btn_roll_dice.disabled = true;
     btn_hold.disabled = true;
 
     if (score1 >= winner_score) {
-      player_1.classList.add('player--active', 'player--winner')
-      player_2.classList.remove('player--active')
+      player_1.classList.add('player--active', 'player--winner');
+      player_2.classList.remove('player--active');
     } else if (score2 >= winner_score) {
-      player_2.classList.add('player--active', 'player--winner')
-      player_1.classList.remove('player--active')
+      player_2.classList.add('player--active', 'player--winner');
+      player_1.classList.remove('player--active');
     }
   }
 }
 
-
 // add click event to roll the dice button.
 btn_roll_dice.addEventListener('click', function () {
-  
   // counter for the interval
   let counter = 0;
 
@@ -91,9 +88,9 @@ btn_roll_dice.addEventListener('click', function () {
     // random number for the result of the dice.
     const random_number = Math.floor(Math.random() * 6);
     dice_image.src = dice_array[random_number];
-    dice_image.style.display = "block";
+    dice_image.style.display = 'block';
 
-    counter++
+    counter++;
 
     if (counter === 15) {
       // enable so now you can click one of your 2 options.
@@ -110,102 +107,87 @@ btn_roll_dice.addEventListener('click', function () {
           score = 0;
 
           //styling
-          player_1.classList.remove('player--active')
-          player_2.classList.add('player--active')
+          player_1.classList.remove('player--active');
+          player_2.classList.add('player--active');
 
-          active_player.player_1 = false
+          active_player.player_1 = false;
           active_player.player_2 = true;
-
         } else {
           current_score_player_1.textContent = score;
         }
-
       } else if (active_player.player_2) {
         if (random_number === 5) {
           current_score_player_2.textContent = 0;
           score = 0;
 
           //styling
-          player_1.classList.add('player--active')
-          player_2.classList.remove('player--active')
+          player_1.classList.add('player--active');
+          player_2.classList.remove('player--active');
 
           active_player.player_1 = true;
           active_player.player_2 = false;
         } else {
           current_score_player_2.textContent = score;
         }
-        
       }
       console.log(score);
     }
-
   }, 80);
-
 });
 
-
-
 btn_hold.addEventListener('click', function () {
-
-  if (score > 0 ) {
-
-    if(active_player.player_1) {
-
+  if (score > 0) {
+    if (active_player.player_1) {
       // Haal de huidige totale score op en zet deze om naar een integer
-      let totalScore = parseInt(score_player_1.textContent); 
-      totalScore += score; 
+      let totalScore = parseInt(score_player_1.textContent);
+      totalScore += score;
       // add deze aan de textcontent
-      score_player_1.textContent = totalScore; 
-  
-  
+      score_player_1.textContent = totalScore;
+
       //styling
-      player_1.classList.remove('player--active')
-      player_2.classList.add('player--active')
-  
-      active_player.player_1 = false
+      player_1.classList.remove('player--active');
+      player_2.classList.add('player--active');
+
+      active_player.player_1 = false;
       active_player.player_2 = true;
-  
+
       score = 0;
       current_score_player_1.textContent = 0;
-  
     } else if (active_player.player_2) {
-  
       // Haal de huidige totale score op en zet deze om naar een integer
-      let totalScore = parseInt(score_player_2.textContent); 
+      let totalScore = parseInt(score_player_2.textContent);
       totalScore += score;
       // add deze aan de textcontent
       score_player_2.textContent = totalScore;
-  
+
       //styling
-      player_1.classList.add('player--active')
-      player_2.classList.remove('player--active')
-  
+      player_1.classList.add('player--active');
+      player_2.classList.remove('player--active');
+
       active_player.player_1 = true;
       active_player.player_2 = false;
-  
+
       score = 0;
       current_score_player_2.textContent = 0;
     }
   }
-  
-  winGame()
+
+  winGame();
 });
-
-
 
 const newGame = function () {
   score_player_1.textContent = 0;
   score_player_2.textContent = 0;
 
   active_player.player_1 = true;
-  player_1.classList.add('player--active')
-  player_1.classList.remove('player--winner')
-  player_2.classList.remove('player--active', 'player--winner')
+  player_1.classList.add('player--active');
+  player_1.classList.remove('player--winner');
+  player_2.classList.remove('player--active', 'player--winner');
 
-  dice_image.style.display = "none";
+  dice_image.style.display = 'none';
 
   btn_roll_dice.disabled = false;
   btn_hold.disabled = false;
-}
+};
 
-btn_newgame.addEventListener('click', newGame)
+btn_newgame.addEventListener('click', newGame);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// hoisting
+
+// Function declaration
+hoistedFunction(); // Outputs: "This function has been hoisted."
+
+function hoistedFunction() {
+  console.log('This function has been hoisted.');
+}
+
+// Function expressions
+// using var
+hoistedFunctionExpr(); // TypeError: hoistedFunctionExpr is not a function
+
+var hoistedFunctionExpr = function() {
+  console.log('Will this function be hoisted?');
+};
+
+// using let or const
+hoistedFunctionExprLet(); // ReferenceError: Cannot access 'hoistedFunctionExprLet' before initialization
+
+let hoistedFunctionExprLet = function() {
+  console.log('Will this function be hoisted with let?');
+};
+
+// arrow funciton works the same as a function expression. depends on the var, const or let.
+arrowFunction(); // TypeError: is not a function
+
+var arrowFunction = () => {
+    console.log('This arrow function has been defined.');
+};
