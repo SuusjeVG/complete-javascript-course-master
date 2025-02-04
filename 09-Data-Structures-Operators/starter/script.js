9edd4c4d118119973cda178918feed2b5bd4f395
Susan <suusjevanglabbeek@live.nl>
2025-02-04 13:21:08 +0100

    test

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -528,3 +528,156 @@ Some of the book objects from the books array are missing the edition
 property. Loop over the books array, and assign this property with a 
 number 1 (if it doesn't already exist). Use logical assignment operators.
 ********/
+
+
+/******************
+Coding challange 1
+******************/
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+  'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};  
+
+
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+const { players } = game;
+const [players1, players2] = players;
+
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+
+// team1: 'Bayern Munich',
+const [gk1] = players1
+const [, ...fieldplayers1] = players1;
+
+// team2: 'Borrussia Dortmund',
+const [gk2] = players2;
+const [, ...fieldplayers2] = players2;
+
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+
+const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic']
+
+// 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+
+const {team1, x: draw, team2} = game.odds;
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+
+const printGoals = function(...players) {
+    console.log(players);
+    console.log(`${players.length} goals were scored`);
+}
+
+// printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels');
+printGoals(...game.scored);
+
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+
+console.log(`${game.team1} is more likely to win because their odds are ${team1}`);
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
+/******************
+Coding Challenge #2
+******************/
+// Let's continue with our football betting app! Keep using the 'game' variable from
+// before.
+
+// Your tasks:
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
+for (let [number, goalscorer] of game.scored.entries()) {
+  console.log(`the ${number += 1} goal is scored by ${goalscorer}`);
+}
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) {
+    average += odd;
+}
+average = average / odds.length;
+console.log(average);
+
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names 😉
+
+for ( const {team1, team2} of Object.values(game)) {
+    console.log(team2);
+}
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+// Lewandowski: 2
+// }
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+console.log(oneWord('Hello World susan'));
