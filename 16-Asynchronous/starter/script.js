f3aa12bd2f8eaabde85b5f55e827baa093e584ea
Susan <suusjevanglabbeek@live.nl>
2025-03-16 22:04:40 +0100

    Started with async section

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,13 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest()
+// request.open('GET', 'https://countries-api-836d.onrender.com/countries/')
+request.open('GET', 'https://restcountries.com/v3.1/name/netherlands')
+request.send()
+
+request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    const data = JSON.parse(this.responseText)
+    console.log(data);
+})
